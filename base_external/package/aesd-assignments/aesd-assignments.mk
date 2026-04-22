6b80107dae8ca62e86f21378a41c08855a423adf
Mohamed Soubhi <eng.mohamed.soubhi@gmail.com>
2026-04-22 03:04:53 +0200

    feat: use aesdsocket-start-stop from server/ dir, update commit hash to f8d2a49

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -5,7 +5,7 @@
 #
 ##############################################################
 
-AESD_ASSIGNMENTS_VERSION = da3e1eb4a474d17bba201735032d032bc13620b0
+AESD_ASSIGNMENTS_VERSION = f8d2a49ecd716dcdeeb48f70cc8f727adaf1da15
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 AESD_ASSIGNMENTS_SITE = git@github.com:mohamed-soubhi/Assignment5Part1_MSoubhi.git
@@ -19,7 +19,7 @@ endef
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin/aesdsocket
 	$(INSTALL) -d 0755 $(TARGET_DIR)/etc/init.d
-	$(INSTALL) -m 0755 $(AESD_ASSIGNMENTS_PKGDIR)/S99aesdsocket $(TARGET_DIR)/etc/init.d/S99aesdsocket
+	$(INSTALL) -m 0755 $(@D)/server/aesdsocket-start-stop $(TARGET_DIR)/etc/init.d/S99aesdsocket
 endef
 
 $(eval $(generic-package))
